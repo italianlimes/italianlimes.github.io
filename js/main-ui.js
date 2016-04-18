3b67cb4d1922286eb78378eb62ea85d70c22be35
aarongillett <mail@aarongillett.com>
2016-04-18 15:33:31 +0200

    About tab: switching
    Fixed tab switching bug by moving js script to about-panel.html

--- js/main-ui.js
+++ js/main-ui.js
@@ -68,17 +68,6 @@ jQuery(document).ready(function($){
 		}
 	});
 
-// SIDE PANEL TABS //
-
-	$('ul.tabs li').click(function(){
-		var tab_id = $(this).attr('data-tab');
-
-		$('ul.tabs li').removeClass('current');
-		$('.tab-content').removeClass('current');
-
-		$(this).addClass('current');
-		$("#"+tab_id).addClass('current');
-	});
 
 // BASTA //
 
